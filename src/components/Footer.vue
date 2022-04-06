84515a7a8afaaef8c6f1397e3f1276cda2117556
sczhaoqi <root@localhost.localdomain>
2022-04-06 15:01:26 +0800

    update pic

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="footer">
-        <p><a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=51012202001089"><img class="alignnone size-full wp-image-628" src="https://www.sczhaoqi.com/wp-content/uploads/2021/08/备案图标.png" alt="" width="20" height="20">公安备案号51012202001089</a>|<a href="https://beian.miit.gov.cn/">蜀ICP备2021025163号</a>-1</p>
+        <p><a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=51012202001089"><img class="alignnone size-full wp-image-628" src="https://www.sczhaoqi.com/uploads/2021/08/备案图标.png" alt="" width="20" height="20">公安备案号51012202001089</a>|<a href="https://beian.miit.gov.cn/">蜀ICP备2021025163号</a>-1</p>
     </div>
 </template>
 
