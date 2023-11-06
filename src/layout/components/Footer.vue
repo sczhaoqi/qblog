11419a7ed0da4eb5e562775583e715f458d409e9
root <1151324240@qq.com>
2023-11-06 14:45:59 +0800

    upd: ipv6

--- src/layout/components/Footer.vue
+++ src/layout/components/Footer.vue
@@ -1,8 +1,7 @@
 <template>
 	<div id="page-footer">
-		<p><a href="https://ipw.cn/ipv6webcheck/" target="_blank">本站支持 IPv6 访问</a></p>
-		<p>
-			<a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=51012202001468"
+		<p><a href="https://ipw.cn/ipv6webcheck/?site=sczhaoqi.ink" title="本站支持IPv6访问" target='_blank'><img style='display:inline-block;vertical-align:middle' alt="本站支持IPv6访问" src="https://static.ipw.cn/icon/ipv6-certified-s1.svg"></a>
+		<a href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=51012202001468"
 				><img class="alignnone size-full wp-image-628" src="@/assets/gongan.png" alt="" width="20" height="20" />公安备案号51012202001468</a>|<a href="https://beian.miit.gov.cn/">蜀ICP备2021025163号</a>-2
 		</p>
 	</div>
